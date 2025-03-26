460c7b77ce89cb2eadb2de78ba9b3bd6c7149249
DVD314 <gordondmitrii@gmail.com>
2025-03-26 10:57:22 +0300

    update abstract scene method template

--- src/scenes/abstract_scene.py
+++ src/scenes/abstract_scene.py
@@ -1,6 +1,11 @@
+from abc import abstractmethod, ABC
 from objects.abstract_object import AbstractObject
 from vec2 import Vec2
 
-class AbstractScene(AbstractObject):
+class AbstractScene(AbstractObject, ABC):
     def __init__(self):
         super().__init__(Vec2(0, 0))
+    
+    @abstractmethod
+    def update(self) -> str | None:
+        return None
